8e4e7b951efe995fb18cd57fb7466fd2d6e6cb8b
JasonL-HealthTrust <90853794+JasonL-HealthTrust@users.noreply.github.com>
2026-03-02 19:21:38 +0000

    Update settings and URLs to support CODESPACE_NAME for dynamic ALLOWED_HOSTS and API root URL

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-iljzhuy3g-6x)3q1^b6dnp3-n$l_xff@!z5*!!2w(!+t8op8q%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
